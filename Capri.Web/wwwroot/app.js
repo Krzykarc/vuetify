a311f97079f6ffd1a9c96cc26b6348fe29b0b9a6
Krzyk <krzykarc@gmail.com>
2019-09-05 14:17:54 +0200

    31/vuetify_problem

--- Capri.Web/wwwroot/app.js
+++ Capri.Web/wwwroot/app.js
@@ -1,10 +1,15 @@
 import App from '@src/app.vue';
 import { Vue } from 'vue-property-decorator';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.scss';
+import 'material-design-icons-iconfont/dist/material-design-icons.scss';
+Vue.use(Vuetify);
 if (module['hot']) {
     module['hot'].accept();
 }
 new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
     render: function (h) { return h(App); },
 });
 //# sourceMappingURL=app.js.map
